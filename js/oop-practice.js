1bee2e013a97c1e460604a61fb746fbd7fd92e2c
Brittany Anderson <142555405+brittanyanderson1@users.noreply.github.com>
2025-01-25 12:09:54 -0800

    Step 1 OOP Practice exercise complete

--- js/oop-practice.js
+++ js/oop-practice.js
@@ -1,6 +1,21 @@
 // STEP 1
+class cat {
+    constructor(name, age) {
+        this.name = name
+    }
+}
 
+const myCat = new cat ('Midnight')
+
+const dog = class {
+    constructor(name) {
+        this.name = name
+    }
+}
+
+const myDog = new dog ('Odie')
 // STEP 2
+
 
 // STEP 3
 
