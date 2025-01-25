feb6a5f5939c625e084e655916fe6b3242d02632
Brittany Anderson <142555405+brittanyanderson1@users.noreply.github.com>
2025-01-25 13:20:49 -0800

    Step 5 OOP Practice exercise tested and completed.

--- js/oop-practice.js
+++ js/oop-practice.js
@@ -45,19 +45,31 @@
 //myAnimal.messageDisplayed()
 
 // STEP 4
+//class Animal {
+//    constructor(message) {
+//        this.message = message
+//    }
+//    displayMessage() {
+//        console.log(this.message)
+//    }
+//}
+
+//const myAnimal = new Animal('The Animal has been created.')
+//myAnimal.displayMessage()
+
+// STEP 5
 class Animal {
-    constructor(message) {
-        this.message = message
-    }
-    displayMessage() {
-        console.log(this.message)
+    constructor(type, breed, color, height, length) {
+        this.type = type
+        this.breed = breed
+        this.color = color
+        this.height = height
+        this.length = length
     }
 }
 
-const myAnimal = new Animal('The Animal has been created.')
-myAnimal.displayMessage()
-
-// STEP 5
+const myAnimal = new Animal('Gerbil', 'Fat Tailed', 'Gray', '4-in', '2-in')
+console.log(myAnimal)
 
 // STEP 6
 
