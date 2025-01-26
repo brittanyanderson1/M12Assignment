dc4021e534f9250d15419f0b0c5f594e8d0d3ec9
Brittany Anderson <142555405+brittanyanderson1@users.noreply.github.com>
2025-01-25 17:26:23 -0800

    Tested and completed Player assignment this is the final commit.

--- js/player.js
+++ js/player.js
@@ -1,6 +1,6 @@
 class Jukebox {
-    constructor(albums) {
-        this.albums = []
+    constructor(albums = []) {
+        this.albums = albums
     }
     addAlbum = function(album) {
         this.albums.push(album)
@@ -13,7 +13,7 @@ class Jukebox {
                 fav = this.albums[i]
             }
         }
-        return fav.display()
+        return fav
     }
 }
 
@@ -30,15 +30,43 @@ class Album {
         return `${this.artist} : ${this.title}. The album has been played ${this.played} times.`
     }
 }
-
-var jbox = new Jukebox()
-const album1 = new Album('Operation Ivy', 'Energy')
-const album2 = new Album('Blink 182', 'Dude Ranch')
-const album3 = new Album('New Found Glory', 'Sticks and Stones')
+const albums = []
+var jbox = new Jukebox(albums)
+const album1 = new Album('Red Hot Chilli Peppers', 'Californiacation')
+const album2 = new Album('Blink 182', 'All the Small Things')
+const album3 = new Album('Puddle of Mud', 'Blurry')
 
 jbox.addAlbum(album1)
 jbox.addAlbum(album2)
 jbox.addAlbum(album3)
+
+
+const albumSelect = document.getElementById('albumSelect')
+    albums.forEach((album, index) => {
+      const option = document.createElement('option')
+      option.value = index
+      option.text = `${album.artist} - ${album.title}`
+      albumSelect.appendChild(option)
+    })
+
+    const playButton = document.getElementById('playButton')
+    const showFavoriteButton = document.getElementById('showFavoriteButton')
+    const favoriteAlbumDiv = document.getElementById('favoriteAlbum')
+
+    playButton.addEventListener('click', () => {
+      const selectedIndex = albumSelect.value
+      jbox.albums[selectedIndex].play()
+      alert(`Playing ${jbox.albums[selectedIndex].title} by ${jbox.albums[selectedIndex].artist}. Played ${jbox.albums[selectedIndex].played} times.`)
+    })
+
+    showFavoriteButton.addEventListener('click', () => {
+      const favorite = jbox.favoriteAlbum()
+      if(favorite) {
+        favoriteAlbumDiv.innerText = `Your favorite album is "${favorite.title}" by ${favorite.artist} with ${favorite.played} plays.`
+    }else{
+        favoriteAlbumDiv.innerText = "No albums have been played yet."
+    }
+    })
 
 album1.play()
 album2.play()
